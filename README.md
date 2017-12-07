a7543f19998188e17bee1b478f2c85df2cefe204
dennieb <dennieford@hotmail.com>
2017-12-07 12:09:02 +0100

    changed map structure

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # practicum-4.2
 Hanze coure 4.2 ICT WAD
+
+Week 1: Introductie, technieken en EAI
+Week 2: Ondersteunde tools en technieken
+Week 3: Gedistribueerde Applicaties aan elkaar koppelen: Het boetesysteem
---
+++ week1/Opdracht_RMI3/RMIHelloStub/nl/hanze/web/rmi/Hello.java
@@ -0,0 +1,11 @@
+package nl.hanze.web.rmi;
+
+import java.rmi.*;
+
+public interface Hello extends Remote {
+    public String sayHello() throws RemoteException;
+    public String sayHello(String name) throws RemoteException;
+    public String sayHello(int age) throws RemoteException;
+    public String sayHello(String name, int age) throws RemoteException;
+    public int ageNextYear(int age) throws RemoteException;
+}
